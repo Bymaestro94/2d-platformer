944278ce3d853da42aca78a6a10bf6a865e24391
Arsen kar <bymaestrowork94@gmail.com>
2024-10-16 20:13:50 +0300

    update
    update

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private float moveSpeed;
+
+    private float xInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();  
+        anim = GetComponentInChildren<Animator>();     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xInput = Input.GetAxis("Horizontal");
+
+        HandleAnimation();
+        HandleMovement();
+    }
+
+    private void HandleAnimation(){
+        anim.SetBool("IsRunning", rb.velocity.x != 0);
+    }
+    private void HandleMovement(){
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
     }
 }
